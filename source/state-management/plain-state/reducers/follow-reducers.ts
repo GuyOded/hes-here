1e2fdaefe21311718ead6e81040bbd81bd1718f2
Guy <israelonion@gmail.com>
2021-01-22 11:28:40 +0200

    Finish reducer implementation
    Refrain from using concat and splice this time

--- source/state-management/plain-state/reducers/follow-reducers.ts
+++ source/state-management/plain-state/reducers/follow-reducers.ts
@@ -40,12 +40,27 @@ const unfollowReducer: StateTemplateReducer = {
         if (action.actionName !== "REMOVE_FOLLOW") {
             return state;
         }
-        const followActionArgs: UnfollowArgs = action.arguments as FollowArgs;
-        const flattenedFollowArgs: UnfollowArgs = {
-            ...followActionArgs,
+        const unfollowActionArgs: UnfollowArgs = action.arguments as FollowArgs;
+        const flattenedUnfollowArgs: UnfollowArgs = {
+            ...unfollowActionArgs,
             // TODO: Write a utility function for removing duplicates
-            members: Array.from(new Set([...followActionArgs.members]))
+            members: Array.from(new Set([...unfollowActionArgs.members]))
         }
+
+        
+        let newState: StateTemplate = [...state];
+        const userStateIndex: number = newState.findIndex((userState: UserState) => {
+            return userState.id === action.invoker;
+        });
+        if (userStateIndex === -1) {
+            return state;
+        }
+
+        newState[userStateIndex].following = newState[userStateIndex].following.filter((currentlyFollowing: string) => {
+            return !flattenedUnfollowArgs.members.includes(currentlyFollowing);
+        });
+
+        return newState;
     }
 }
 
